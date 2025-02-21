ae2d1fcde89ccffb1d22b099d53fe9b88a636fe5
henriquegomesdev <39200841+henriquegomesdev@users.noreply.github.com>
2025-02-21 18:02:14 -0300

    Ajustando configuração.

--- front-end/ui/src/environments/environment.ts
+++ front-end/ui/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8090' // Substitua pelo endereço da sua API
+  apiUrl: 'http://localhost:8090/ngspring' // Substitua pelo endereço da sua API
 };
